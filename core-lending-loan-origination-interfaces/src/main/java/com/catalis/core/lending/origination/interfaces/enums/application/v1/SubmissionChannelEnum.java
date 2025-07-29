30b30e338fc448a069cc423991aafaebcce8816e
Andrés Contreras Guillén <andres.contreras@getfirefly.io>
2025-07-29 10:04:21 +0200

    feat(interfaces): update loan application DTO and submission channel enum

--- core-lending-loan-origination-interfaces/src/main/java/com/catalis/core/lending/origination/interfaces/enums/application/v1/SubmissionChannelEnum.java
+++ core-lending-loan-origination-interfaces/src/main/java/com/catalis/core/lending/origination/interfaces/enums/application/v1/SubmissionChannelEnum.java
@@ -3,5 +3,6 @@ package com.catalis.core.lending.origination.interfaces.enums.application.v1;
 public enum SubmissionChannelEnum {
     BRANCH,
     ONLINE,
-    MOBILE
+    MOBILE,
+    DISTRIBUTOR
 }
